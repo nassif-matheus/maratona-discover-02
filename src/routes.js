7c7582d53f07e95f2521912b1b285ae13a7cbf35
Matheus Nassif <matheus.nassifsc@gmail.com>
2021-10-04 16:42:27 -0300

    Maratona Discover 02

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = express.Router()
+const ProfileController = require('./controllers/ProfileController')
+const JobController = require('./controllers/JobController')
+const DashboardController = require('./controllers/DashboardController')
+// request, response
+routes.get('/', DashboardController.index)
+routes.get('/job/:id', JobController.show)
+routes.post('/job/:id', JobController.update)
+routes.post('/job/delete/:id', JobController.delete)
+routes.post('/job', JobController.save)
+routes.get('/job', JobController.create)
+routes.get('/profile', ProfileController.index)
+routes.post('/profile', ProfileController.update)
+
+module.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+const path = require("path")
+
+server.set('view engine', 'ejs')
+
+// Mudar a localização da pasta views
+server.set('views', path.join(__dirname, 'views'))
+// Habilitar arquivos statics
+server.use(express.static("public"))
+
+
+//Usar o req.body
+server.use(express.urlencoded({ extended: true }))
+// Routes
+server.use(routes)
+
+server.listen(3000, () => console.log('Running'))
